723b982d6565dd4269f10b5ff95ac1031db6994e
Abiyyu Yafi <luckyveri3@gmail.com>
2021-04-02 15:03:28 +0700

    Add celery task

---
+++ project/tasks/tasks.py
@@ -0,0 +1,31 @@
+from project import celery
+from project.service.ethereum_service import EthereumService
+from time import sleep
+from dotenv import load_dotenv
+import os
+
+es = EthereumService()
+load_dotenv()
+
+@celery.task
+def SaveAdminToContract(admin_address, signature,ketua_nonce):
+    w3 = es.SetupW3()
+    contract = es.AccessContract()
+    main_acc_access = os.getenv("MAIN_ACCOUNT")
+    main_acc_address = w3.eth.account.privateKeyToAccount(main_acc_access).address
+    main_nonce = w3.eth.getTransactionCount(
+        w3.toChecksumAddress(main_acc_address)
+    )
+    tx_hash = contract.functions.addAdminPetugas(
+        w3.toChecksumAddress(admin_address),
+        ketua_nonce,
+        signature
+    ).buildTransaction({
+        "chainId":5777,
+        "gas":70000,
+        "gasPrice":w3.toWei('1','gwei'),
+        "nonce":main_nonce
+    })
+    sign_tx = w3.eth.account.sign_transaction(tx_hash,main_acc_access)
+    w3.eth.sendRawTransaction(sign_tx.rawTransaction)
+    return w3.toHex(w3.keccak(sign_tx.rawTransaction))
