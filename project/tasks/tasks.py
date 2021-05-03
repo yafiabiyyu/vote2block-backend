5ce299808a87a9e4cb5e4c836cf24fda2910bfbc
Abiyyu Yafi <luckyveri3@gmail.com>
2021-05-04 05:32:01 +0700

    menambahkan task hapus petugas dari contract & mengganti admin petugas menjadi petugas

--- project/tasks/tasks.py
+++ project/tasks/tasks.py
@@ -8,7 +8,7 @@ es = EthereumService()
 load_dotenv()
 
 @celery.task
-def SaveAdminToContract(admin_address, signature,ketua_nonce):
+def SavePetugasToContract(admin_address, signature,ketua_nonce):
     w3 = es.SetupW3()
     contract = es.AccessContract()
     main_acc_access = os.getenv("MAIN_ACCOUNT")
@@ -16,7 +16,7 @@ def SaveAdminToContract(admin_address, signature,ketua_nonce):
     main_nonce = w3.eth.getTransactionCount(
         w3.toChecksumAddress(main_acc_address)
     )
-    tx_hash = contract.functions.addAdminPetugas(
+    tx_hash = contract.functions.addPetugas(
         w3.toChecksumAddress(admin_address),
         ketua_nonce,
         signature
@@ -27,5 +27,26 @@ def SaveAdminToContract(admin_address, signature,ketua_nonce):
         "nonce":main_nonce
     })
     sign_tx = w3.eth.account.sign_transaction(tx_hash,main_acc_access)
+    w3.eth.sendRawTransaction(sign_tx.rawTransaction)
+    return w3.toHex(w3.keccak(sign_tx.rawTransaction))
+
+@celery.task
+def RemovePetugasFromContract(admin_address, signature, ketua_nonce):
+    w3 = es.SetupW3()
+    contract = es.AccessContract()
+    main_acc_access = os.getenv("MAIN_ACCOUNT")
+    main_acc_address = w3.eth.account.privateKeyToAccount(main_acc_access).address
+    main_nonce = w3.eth.getTransactionCount(main_acc_address)
+    tx_hash = contract.functions.removePetugas(
+        w3.toChecksumAddress(admin_address),
+        ketua_nonce,
+        signature
+    ).buildTransaction({
+        "chainId":5777,
+        "gas":70000,
+        "gasPrice":w3.toWei('1', 'gwei'),
+        "nonce":main_nonce
+    })
+    sign_tx = w3.eth.account.sign_transaction(tx_hash, main_acc_access)
     w3.eth.sendRawTransaction(sign_tx.rawTransaction)
     return w3.toHex(w3.keccak(sign_tx.rawTransaction))
