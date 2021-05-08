4aad3b54a14182d1629bbafdf8574055d8eb1507
Abiyyu Yafi <luckyveri3@gmail.com>
2021-05-09 00:53:25 +0700

    update service ketua untuk mengatur petugas

--- project/service/ketua_service.py
+++ project/service/ketua_service.py
@@ -2,7 +2,7 @@ from project.models.user_model import UserDoc
 from project.service.enkripsi_service import DataEncryption
 from project.service.ethereum_service import EthereumService
 from project.service.user_info_service import UserInfo
-from project.tasks.tasks import SaveAdminToContract
+from project.tasks.tasks import SavePetugasToContract,RemovePetugasFromContract
 from eth_account.messages import encode_defunct
 from celery.result import AsyncResult
 from datetime import datetime
@@ -12,65 +12,138 @@ es = EthereumService()
 us = UserInfo()
 
 class KetuaService:
-    def GenerateUsername(self,data):
-        username = data.replace(" ","")
+    def GenerateUsername(self, data):
+        username = data.replace(" ", "")
         return username.lower()
     
-    def AddAdminPetugas(self, json_data, user_data):
+    def AddPetugas(self, json_data, user_data):
         w3 = es.SetupW3()
-        admin_address, admin_access = es.CreateWallet()
+        petugas_address, petugas_access = es.CreateWallet()
         ketua_address, ketua_access = us.GetUserData(user_data)
-        admin_data = UserDoc.objects(nama_lengkap=json_data['nama_lengkap']).first()
+        petugas_data = UserDoc.objects(nama_lengkap=json_data['nama_lengkap']).first()
         username = self.GenerateUsername(json_data['nama_lengkap'])
         access = {
-            "level":"1",
-            "group":"penyelenggara"
+            "level":"petugas",
+            "status":"aktif"
         }
-        if admin_data is None:
+        if petugas_data is None:
             try:
-                save_admn = UserDoc(
+                save_petugas = UserDoc(
                     username=username,
                     nama_lengkap=json_data['nama_lengkap'],
                     contact=json_data['contact'],
                     alamat=json_data['alamat'],
                     access=access,
-                    ethereum={
-                        "ethereum_address":admin_address,
-                        "ethereum_access":admin_access
+                    ethereum = {
+                        "ethereum_address":petugas_address,
+                        "ethereum_access":petugas_access.decode()
                     }
                 )
-                save_admn.GeneratePasswordHash(username)
-                save_admn.save()
+                save_petugas.GeneratePasswordHash(username)
+                save_petugas.save()
 
-                # Create signature message
-                ketua_nonce = w3.eth.getTransactionCount(w3.toChecksumAddress(ketua_address))
+                # create signature message
+                ketua_nonce = w3.eth.getTransactionCount(ketua_address)
                 msg = w3.soliditySha3(
-                    ['address','uint256'],
+                    ['address', 'uint256'],
                     [
-                        w3.toChecksumAddress(admin_address),
+                        w3.toChecksumAddress(petugas_address),
                         ketua_nonce
                     ]
                 )
                 message = encode_defunct(primitive=msg)
-                sign_message = w3.eth.account.sign_message(message,ketua_access)
-                result = SaveAdminToContract.delay(
-                    admin_address,
+                sign_message = w3.eth.account.sign_message(message, ketua_access)
+                result = SavePetugasToContract.delay(
+                    w3.toChecksumAddress(petugas_address),
                     sign_message.signature.hex(),
                     ketua_nonce
                 )
-                sign_hex = sign_message.signature.hex()
-                us.SaveUserTx(user_data, result.wait(),sign_hex)
+                us.SaveUserTx(user_data, result.wait(), sign_message.signature.hex())
                 message_object = {
                     "status":"Berhasil",
-                    "admin_address":admin_address,
-                    "signature":sign_hex
+                    "message":"Petugas {} berhasil ditambahkan".format(json_data['nama_lengkap'])
                 }
                 return message_object
             except Exception as e:
-                return {"status":"Error","message":e}
+                message_object = {
+                    "status":"Error",
+                    "message":e
+                }
+                return message_object
         else:
             message_object = {
                 "status":"Gagal",
-                "message":"User telah terdaftar"
+                "message":"Pengguna {} telah terdaftar".format(json_data['nama_lengkap'])
             }
             return message_object
+    
+    def RemovePetugas(self, petugasId, user_data):
+        w3 = es.SetupW3()
+        ketua_address, ketua_access = us.GetUserData(user_data)
+        get_petugas_data = UserDoc.objects(id=str(petugasId)).first()
+        access = {
+            "level":"None",
+            "status":"nonaktif"
+        }
+        if get_petugas_data.access['status'] == "aktif":
+            try:
+                # update access petugas menjadi nonaktif
+                remove_admin = UserDoc.objects(id=str(petugasId)).update(access=access)
+
+                # membuat signature message menghapus petugas dari smartcontract
+                ketua_nonce = w3.eth.getTransactionCount(w3.toChecksumAddress(ketua_address))
+                msg = w3.soliditySha3(
+                    ['address', 'uint256'],
+                    [
+                        w3.toChecksumAddress(get_petugas_data.ethereum['ethereum_address']),
+                        ketua_nonce
+                    ]
+                )
+                message = encode_defunct(primitive=msg)
+                sign_message = w3.eth.account.sign_message(message, ketua_access)
+                result = RemovePetugasFromContract.delay(
+                    w3.toChecksumAddress(get_petugas_data.ethereum['ethereum_address']),
+                    sign_message.signature.hex(),
+                    ketua_nonce
+                )
+                us.SaveUserTx(user_data, result.wait(), sign_message.signature.hex())
+                message_object = {
+                    "status":"Berhasil",
+                    "message":"Petugas {} berhasil di hapus".format(get_petugas_data.nama_lengkap)
+                }
+                return message_object
+            except Exception as e:
+                message_object = {
+                    "status":"Error",
+                    "message":e
+                }
+        else:
+            message_object = {
+                "status":"Gagal",
+                "message":"Petugas {} tidak terdaftar dalam sistem".format(get_petugas_data.nama_lengkap)
+            }
+            return message_object
+    
+    def GetAllPetugas(self):
+        try:
+            data_list = []
+            data = UserDoc.objects(access={"level":"petugas","status":"aktif"}).all()
+            for user in data:
+                data_list.append({
+                    "id":user.id,
+                    "nama_lengkap":user.nama_lengkap,
+                    "status":user.access['status'],
+                    "level":user.access['level'],
+                    "ethereum_address":user.ethereum['ethereum_address']
+                })
+            return data_list
+        except Exception as e:
+            message_object = {
+                "status":"Error",
+                "message":e
+            }
+            return e
+    
+    def GetOnePetugas(self, petugasId):
+        user = UserDoc.objects(id=str(petugasId)).first()
+        return user
