e0fa4c2bf7edf6f47e0af31b7addf11365ce537e
Abiyyu Yafi <luckyveri3@gmail.com>
2021-06-04 14:23:36 +0700

    Perubahan pada pada logic petugas service

--- project/service/petugas_service.py
+++ project/service/petugas_service.py
@@ -1,9 +1,9 @@
 from project.models.voting_model import Kandidat, VotingTimeStamp
 from project.service.ethereum_service import EthereumService
+from web3.exceptions import SolidityError
 from project.service.user_info_service import UserInfo
-from eth_account.message import encode_defunct
+from eth_account.messages import encode_defunct
 from project.tasks.tasks import RegisterKandidat
-from datetime import datetime
 import time
 
 es = EthereumService()
@@ -11,79 +11,101 @@ us = UserInfo()
 
 
 class PetugasService:
-    def GenerateBytesNama(self, nama_kandidat):
+    def ConvertKandidatNameToBytes(self, kandidat_name):
         w3 = es.SetupW3()
-        name_bytes = w3.soliditySha3(["string"], [str(nama_kandidat)])
-        return name_bytes
+        bytes_name = w3.soliditySha3(["string"], [str(kandidat_name)])
+        return bytes_name.hex()
 
-    def CheckRegisterTimeStamp(self):
+    def CheckRegisterTIme(self):
         current_timestamp = time.time()
-        get_timestamp = VotingTimeStamp.objects().all()
-        if (
-            current_timestamp > get_timestamp.register_start
-            and current_timestamp > get_timestamp.register_finis
+        get_timestamp_data = VotingTimeStamp.objects().all()
+        if current_timestamp < get_timestamp_data.register_start:
+            message_object = {
+                "status": "Gagal",
+                "message": "Pendaftaran Kandidat dan Pemilih belum dibuka",
+            }
+            return message_object
+        elif (
+            current_timestamp > get_timestamp_data.register_start
+            and current_timestamp < get_timestamp_data.register_finis
         ):
-            return True
+            message_object = {
+                "status": "Berhasil",
+                "message": "Pendaftaran Kandidat dan Pemilih telah dibuka",
+            }
+            return message_object
         else:
-            return False
+            message_object = {
+                "status": "Gagal",
+                "message": "Pendaftaran Kandidat dan Pemilih telah ditutup",
+            }
+            return message_object
 
     def RegisterKandidat(self, json_data, user_data):
         w3 = es.SetupW3()
         petugas_address, petugas_access = us.GetUserData(user_data)
-        get_kandidat_data = Kandidat.objects(
+        check_register_time = self.CheckRegisterTIme()
+        check_kandidat_data = Kandidat.objects(
             nama_kandidat=json_data["nama_kandidat"]
-        ).first()
-        bytes_name = self.GenerateBytesNama(json_data["nama_kandidat"])
-        if getkandidat_data is None:
-            try:
-                petugas_nonce = w3.eth.getTransactionCount(
-                    w3.toChecksumAddress(petugas_address)
-                )
-                msg = w3.soliditySha3(
-                    ["uint256", "bytes32", "uint256"],
-                    [
+        )
+        if check_register_time == "Berhasil":
+            if check_kandidat_data is None:
+                try:
+                    nonce = w3.eth.getTransactionCount(petugas_address)
+                    bytes_kandidat_name = (
+                        self.ConvertKandidatNameToBytes(
+                            json_data["nama_kandidat"]
+                        )
+                    )
+                    msg = w3.soliditySha3(
+                        ["uint256", "bytes32", "uint256"],
+                        [
+                            int(json_data["kandidat_id"]),
+                            bytes_kandidat_name,
+                            nonce,
+                        ],
+                    )
+                    message = encode_defunct(primitive=msg)
+                    sign_message = w3.eth.account.sign_message(
+                        message, petugas_access
+                    )
+                    result = RegisterKandidat.delay(
                         int(json_data["kandidat_id"]),
-                        bytes_name,
-                        petugas_none,
-                    ],
-                )
-                message = encode_defunct(primitive=msg)
-                sign_message = w3.eth.account.sign_message(
-                    message, petugas_access
-                )
-                result = RegisterKandidat.delay(
-                    int(json_data["kandidat_id"]),
-                    petugas_nonce,
-                    bytes_name,
-                    sign_message.signature.hex(),
-                )
-                us.SaveUserTx(
-                    user_data,
-                    result.wait(),
-                    sign_message.signature.hex(),
-                )
-            except Exception as e:
-                message_object = {"status": "Error", "message": e}
-                return message_object
+                        nonce,
+                        bytes_kandidat_name,
+                        sign_message.signature.hex(),
+                    )
+                    if result.wait() == "Gagal":
+                        raise SolidityError
+                except SolidityError:
+                    message_object = {
+                        "status": "Error",
+                        "message": "Terjadi masalah pada server",
+                    }
+                    return message_object
+                else:
+                    save_kandidat = Kandidat(
+                        nomor_urut=int(json_data["kandidat_id"]),
+                        nama_kandidat=json_data["nama_kandidat"],
+                        nama_bytes=bytes_kandidat_name,
+                        alamat=json_data["alamat"],
+                        contact=json_data["contact"],
+                        image_url=json_data["image_url"],
+                    )
+                    save_kandidat.save()
+                    us.SaveUserTx(
+                        user_data, result.wait(), sign_message.hex()
+                    )
+                    message_object = {
+                        "status": "Berhasil",
+                        "message": "Kandidat berhasil di daftarkan kedalam sistem",
+                    }
+                    return message_object
             else:
-                save_kandidat_data = Kandidat(
-                    nomor_urut=int(json_data["nomor_kandidat"]),
-                    nama_kandidat=json_data["nama_kandidat"],
-                    nama_bytes=bytes_name.hex(),
-                    alamat=json_data["alamat"],
-                    contact=json_data["contact"],
-                )
-                save_kandidat_data.save()
                 message_object = {
-                    "status": "Berhasil",
-                    "message": "Kandidat berhasil di daftarkan",
+                    "status":"Gagal",
+                    "message":"Kandiat sudah terdaftar di dalam sistem"
                 }
                 return message_object
         else:
-            message_object = {
-                "status": "Gagal",
-                "message": "Kandidat {} Telah terdaftar".format(
-                    json_data["nama_kandidat"]
-                ),
-            }
-            return message_object
+            return check_register_time
