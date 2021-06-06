869723589e255862852a5d40bebbe524bb0ec7cb
Abiyyu Yafi <luckyveri3@gmail.com>
2021-06-07 03:19:05 +0700

    Menambahkan tasks untuk register pemilih dan mengganti beberapa settingan untuk pengaturan gasPrice

--- project/tasks/tasks.py
+++ project/tasks/tasks.py
@@ -120,15 +120,37 @@ def RegisterKandidat(kandidatId, nonce, bytes_name, signature):
         int(kandidatId), nonce, unixtimestamp, bytes_name, signature
     ).buildTransaction(
         {
-            "chainId": 5777,
-            "gas": 70000,
             "gasPrice": w3.toWei("1", "gwei"),
             "nonce": main_nonce,
         }
     )
-    sign_tx = w3.eth.account.sign_transaction(tx_hash.rawTransaction)
+    sign_tx = w3.eth.account.sign_transaction(tx_hash,main_acc_access)
     try:
-        w3.eth.sendRawTransaction(sign_tx, main_acc_access)
+        w3.eth.sendRawTransaction(sign_tx.rawTransaction)
+        return w3.toHex(w3.keccak(sign_tx.rawTransaction))
+    except Exception:
+        return "Gagal"
+
+@celery.task
+def RegisterPemilih(nonce, pemilih_address, signature):
+    w3 = es.SetupW3()
+    contract = es.AccessContract()
+    main_acc_access = os.getenv('MAIN_ACCOUNT')
+    main_acc_address = w3.eth.account.privateKeyToAccount(main_acc_access).address
+    unixtimestamp = int(time.time())
+    main_nonce = w3.eth.getTransactionCount(main_acc_address)
+    tx_hash = contract.functions.registerPemilih(
+        pemilih_address,
+        nonce,
+        unixtimestamp,
+        signature
+    ).buildTransaction({
+        "gasPrice":w3.toWei('1', "gwei"),
+        "nonce":main_nonce
+    })
+    sign_tx = w3.eth.account.sign_transaction(tx_hash, main_acc_access)
+    try:
+        w3.eth.sendRawTransaction(sign_tx.rawTransaction)
         return w3.toHex(w3.keccak(sign_tx.rawTransaction))
     except Exception:
         return "Gagal"
