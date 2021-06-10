b022fe6a19efad6909457848105962987b3c6041
Abiyyu Yafi <luckyveri3@gmail.com>
2021-06-10 17:00:44 +0700

    Memperbarui task

--- project/tasks/tasks.py
+++ project/tasks/tasks.py
@@ -1,6 +1,5 @@
 from project import celery
 from project.service.ethereum_service import EthereumService
-from time import sleep
 from dotenv import load_dotenv
 import os, time
 
@@ -9,28 +8,22 @@ load_dotenv()
 
 
 @celery.task
-def SavePetugasToContract(admin_address, signature, ketua_nonce):
+def SavePetugasTask(petugas_address, signature, ketua_nonce):
     w3 = es.SetupW3()
     contract = es.AccessContract()
-    main_acc_access = os.getenv("MAIN_ACCOUNT")
-    main_acc_address = w3.eth.account.privateKeyToAccount(
-        main_acc_access
+    main_account_access = os.getenv("MAIN_ACCOUNT")
+    main_account_address = w3.eth.account.privateKeyToAccount(
+        main_account_access
     ).address
-    w3.eth.defaultAccount = main_acc_address
-    main_nonce = w3.eth.getTransactionCount(
-        w3.toChecksumAddress(main_acc_address)
+    main_account_nonce = w3.eth.getTransactionCount(
+        main_account_address
     )
     tx_hash = contract.functions.addPetugas(
-        w3.toChecksumAddress(admin_address), ketua_nonce, signature
-    ).buildTransaction(
-        {
-            "chainId": 5777,
-            "gas": 70000,
-            "gasPrice": w3.toWei("1", "gwei"),
-            "nonce": main_nonce,
-        }
+        w3.toChecksumAddress(petugas_address), ketua_nonce, signature
+    ).buildTransaction({"nonce": main_account_nonce})
+    sign_tx = w3.eth.account.sign_transaction(
+        tx_hash, main_account_access
     )
-    sign_tx = w3.eth.account.sign_transaction(tx_hash, main_acc_access)
     try:
         w3.eth.sendRawTransaction(sign_tx.rawTransaction)
         return w3.toHex(w3.keccak(sign_tx.rawTransaction))
@@ -39,25 +32,22 @@ def SavePetugasToContract(admin_address, signature, ketua_nonce):
 
 
 @celery.task
-def RemovePetugasFromContract(admin_address, signature, ketua_nonce):
+def RemovePetugasTask(petugas_address, signature, ketua_nonce):
     w3 = es.SetupW3()
     contract = es.AccessContract()
-    main_acc_access = os.getenv("MAIN_ACCOUNT")
-    main_acc_address = w3.eth.account.privateKeyToAccount(
-        main_acc_access
+    main_account_access = os.getenv("MAIN_ACCOUNT")
+    main_accout_address = w3.eth.account.privateKeyToAccount(
+        main_account_access
     ).address
-    main_nonce = w3.eth.getTransactionCount(main_acc_address)
+    main_account_nonce = w3.eth.getTransactionCount(main_accout_address)
     tx_hash = contract.functions.removePetugas(
-        w3.toChecksumAddress(admin_address), ketua_nonce, signature
-    ).buildTransaction(
-        {
-            "chainId": 5777,
-            "gas": 70000,
-            "gasPrice": w3.toWei("1", "gwei"),
-            "nonce": main_nonce,
-        }
+        w3.toChecksumAddress(petugas_address),
+        ketua_nonce,
+        signature,
+    ).buildTransaction({"nonce": main_account_nonce})
+    sign_tx = w3.eth.account.sign_transaction(
+        tx_hash, main_account_access
     )
-    sign_tx = w3.eth.account.sign_transaction(tx_hash, main_acc_access)
     try:
         w3.eth.sendRawTransaction(sign_tx.rawTransaction)
         return w3.toHex(w3.keccak(sign_tx.rawTransaction))
@@ -66,37 +56,28 @@ def RemovePetugasFromContract(admin_address, signature, ketua_nonce):
 
 
 @celery.task
-def SetVotingTimeStamp(
-    register_start,
-    register_finis,
-    voting_start,
-    voting_finis,
-    signature,
+def SetupTimeAppTask(
+    registerstart, registerfinis, votingstart, votingfinis, signature
 ):
     w3 = es.SetupW3()
     contract = es.AccessContract()
-    main_acc_access = os.getenv("MAIN_ACCOUNT")
-    main_acc_address = w3.eth.account.privateKeyToAccount(
-        main_acc_access
+    main_account_access = os.getenv("MAIN_ACCOUNT")
+    main_account_address = w3.eth.account.privateKeyToAccount(
+        main_account_access
     ).address
-    main_nonce = w3.eth.getTransactionCount(
-        w3.toChecksumAddress(main_acc_address)
+    main_account_nonce = w3.eth.getTransactionCount(
+        main_account_address
     )
     tx_hash = contract.functions.setVotingTimestampEvent(
-        register_start,
-        register_finis,
-        voting_start,
-        voting_finis,
+        registerstart,
+        registerfinis,
+        votingstart,
+        votingfinis,
         signature,
-    ).buildTransaction(
-        {
-            "chainId": 5777,
-            "gas": 70000,
-            "gasPrice": w3.toWei("1", "gwei"),
-            "nonce": main_nonce,
-        }
+    ).buildTransaction({"nonce": main_account_nonce})
+    sign_tx = w3.eth.account.sign_transaction(
+        tx_hash, main_account_access
     )
-    sign_tx = w3.eth.account.sign_transaction(tx_hash, main_acc_access)
     try:
         w3.eth.sendRawTransaction(sign_tx.rawTransaction)
         return w3.toHex(w3.keccak(sign_tx.rawTransaction))
@@ -104,53 +85,75 @@ def SetVotingTimeStamp(
         return "Gagal"
 
 
+# Petugas Peneyelenggara Task
 @celery.task
-def RegisterKandidat(kandidatId, nonce, bytes_name, signature):
+def RegisterKandidatTask(
+    kandidatID, petugas_nonce, bytes_name_kandidat, signature
+):
     w3 = es.SetupW3()
     contract = es.AccessContract()
-    main_acc_access = os.getenv("MAIN_ACCOUNT")
-    main_acc_address = w3.eth.account.privateKeyToAccount(
-        main_acc_access
+    main_account_access = os.getenv("MAIN_ACCOUNT")
+    main_account_address = w3.eth.account.privateKeyToAccount(
+        main_account_access
     ).address
-    main_nonce = w3.eth.getTransactionCount(
-        w3.toChecksumAddress(main_acc_address)
+    main_account_nonce = w3.eth.getTransactionCount(
+        main_account_address
     )
     unixtimestamp = int(time.time())
     tx_hash = contract.functions.registerKandidat(
-        int(kandidatId), nonce, unixtimestamp, bytes_name, signature
-    ).buildTransaction(
-        {
-            "gasPrice": w3.toWei("1", "gwei"),
-            "nonce": main_nonce,
-        }
+        int(kandidatID),
+        petugas_nonce,
+        unixtimestamp,
+        bytes_name_kandidat,
+        signature,
+    ).buildTransaction({"nonce": main_account_nonce})
+    sign_tx = w3.eth.account.sign_transaction(
+        tx_hash, main_account_access
     )
-    sign_tx = w3.eth.account.sign_transaction(tx_hash,main_acc_access)
     try:
         w3.eth.sendRawTransaction(sign_tx.rawTransaction)
         return w3.toHex(w3.keccak(sign_tx.rawTransaction))
     except Exception:
         return "Gagal"
 
+
 @celery.task
-def RegisterPemilih(nonce, pemilih_address, signature):
+def RegisterPemilihTask(petugas_nonce, pemilih_address, signature):
     w3 = es.SetupW3()
     contract = es.AccessContract()
-    main_acc_access = os.getenv('MAIN_ACCOUNT')
-    main_acc_address = w3.eth.account.privateKeyToAccount(main_acc_access).address
+    main_account_access = os.getenv("MAIN_ACCOUNT")
+    main_account_address = w3.eth.account.privateKeyToAccount(
+        main_account_access
+    ).address
+    main_account_nonce = w3.eth.getTransactionCount(
+        main_account_address
+    )
     unixtimestamp = int(time.time())
-    main_nonce = w3.eth.getTransactionCount(main_acc_address)
     tx_hash = contract.functions.registerPemilih(
-        pemilih_address,
-        nonce,
+        w3.toChecksumAddress(pemilih_address),
+        petugas_nonce,
         unixtimestamp,
-        signature
-    ).buildTransaction({
-        "gasPrice":w3.toWei('1', "gwei"),
-        "nonce":main_nonce
-    })
-    sign_tx = w3.eth.account.sign_transaction(tx_hash, main_acc_access)
+        signature,
+    ).buildTransaction({"nonce": main_account_nonce})
+    sign_tx = w3.eth.account.sign_transaction(
+        tx_hash, main_account_access
+    )
     try:
         w3.eth.sendRawTransaction(sign_tx.rawTransaction)
         return w3.toHex(w3.keccak(sign_tx.rawTransaction))
     except Exception:
         return "Gagal"
+
+# Register and Voting Timestamp Task
+@celery.task
+def GetTimestampDataTask():
+    contract = es.AccessContract()
+    result = contract.functions.getTimestampData().call()
+    return result
+
+# Get Pemilih data dari smart-contract
+@celery.task
+def GetPemilihDataTask(pemilih_address):
+    contract = es.AccessContract()
+    result = contract.functions.getPemilihData(pemilih_address).call()
+    return result
