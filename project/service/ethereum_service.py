2fdddf1996e4ee5e9829f6048f5da2b105c562f2
Abiyyu Yafi <luckyveri3@gmail.com>
2021-06-12 19:42:41 +0700

    fix error contract json tidak terbaca dan melakukan setup default wallet

--- project/service/ethereum_service.py
+++ project/service/ethereum_service.py
@@ -12,16 +12,19 @@ class EthereumService:
     def SetupW3(self):
         rpc_url = os.getenv("RPC_URL")
         w3 = Web3(HTTPProvider(rpc_url))
-        w3.eth.defaultAccount = os.getenv("DEFAULT_ACCOUNT")
+        w3.eth.defaultAccount = w3.eth.account.privateKeyToAccount(
+            os.getenv("MAIN_ACCOUNT")
+        ).address
         return w3
 
     def AccessContract(self):
         w3 = self.SetupW3()
         contract_file = open("project/smart-contract/Vote2Block.json")
-        data = json.loads(contract_file)
+        data = json.load(contract_file)
         abi = data["abi"]
         contract_address = os.getenv("CONTRACT_ADDRESS")
         contract = w3.eth.contract(address=contract_address, abi=abi)
+        return contract
 
     def CreateWallet(self):
         w3 = self.SetupW3()
