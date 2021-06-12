7b71f3e3479f1243b62056725dc8f3ad27671d2a
Abiyyu Yafi <luckyveri3@gmail.com>
2021-06-12 19:42:01 +0700

    fix error data type di model KandidatDoc

--- project/models/voting_model.py
+++ project/models/voting_model.py
@@ -1,14 +1,14 @@
-from typing_extensions import Required
 from project.factory import db
 
 
 class KandidatDoc(db.Document):
     _id = db.StringField(required=True, primary_key=True)
+    nomor_urut = db.IntField(required=True)
     nama = db.StringField(required=True)
     nama_bytes = db.StringField(required=True)
     tgl_lahir = db.StringField(required=True)
     visi = db.StringField(required=True)
     misi = db.StringField(required=True)
-    contact = db.StringField(required=True)
-    alamat = db.StringField(required=True)
+    contact = db.DictField(required=True)
+    alamat = db.DictField(required=True)
     image_url = db.StringField(required=True)
