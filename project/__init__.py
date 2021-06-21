c25e221bf03ab1440883c7dc4c321d37b5f21c76
Abiyyu Yafi <luckyveri3@gmail.com>
2021-06-21 08:49:21 +0700

    persiapan deploy

--- project/__init__.py
+++ project/__init__.py
@@ -2,13 +2,14 @@ from celery import Celery
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
 def make_celery(app_name=__name__):
     broker_url = os.getenv("BROKER")
     return Celery(
         app_name,
-        broker="amqp://yafiabiyyu:yafi2105@localhost/unixuser",
-        backend="mongodb://172.17.0.2/celery_task",
+        broker=os.getenv("BROKER"),
+        backend=os.getenv("MONGO_DB_URI"),
     )
 
 
